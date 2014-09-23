0600d8d72e6384acbb3ec7dd31ef9e26679dde3c
chenxinbo <xinbo.chen@jetcloudtech.com>
2014-09-23 19:38:16 +0800

    9.23 最终版

--- 1.0.0/app/scripts/controllers/ctrMyForm.js
+++ 1.0.0/app/scripts/controllers/ctrMyForm.js
@@ -6,8 +6,9 @@ var MyFormControllers = angular.module('MyFormControllers',[]);
 MyFormControllers.controller('MyFormCtrl',function($scope,$compile,CommonService){
 
     var uriData = '';
+    var emData = '';  //判断数据是否为空
     var nowPage = 0;  //当前页
-    var pageNum = 1;  //每页显示条数
+    var pageNum = 10;  //每页显示条数
     var empty = true;    //是否清空数据  true是||false否
     var contentTab = ""; //Html Table_ID
 
@@ -18,7 +19,7 @@ MyFormControllers.controller('MyFormCtrl',function($scope,$compile,CommonService
 
                  /******************************  加载运行 *************************/
     ctrInit();
-
+    $scope.returnFrom=false;   //隐藏退货表单
      /*************************************************  页面交互事件  ********************************************/
      //下拉框
     $scope.$watch('seleDataTime', function(onVal) {
@@ -75,17 +76,28 @@ MyFormControllers.controller('MyFormCtrl',function($scope,$compile,CommonService
         });
     }
 
-    $scope.returnGoods = function(num1,num2){
-       oid = JSON.stringify(num1);
-       orderNo = JSON.stringify(num2);
-       pcode ="11";
-       pname ="22";
+    $scope.returnGoods = function(rData){
+        $scope.returnFrom=true;
+     //  var rDatas = localStorage.getItem("da");
+     /*   var oid = '58';
+        var orderNo = '2014092000000058';
+        var pcode = '123';
+        var pname = '123';*/
+        uriData = '';
+        CommonService.getAll('order', uriData, function (data) {
+            for(var i=0;i<data.OrderList.length;i++){
+                if(data.OrderList[i][0][0]==rData){
+
+                }
+            }
+        });
     }
 
     //重置
     $scope.tabClear = function(){
         nowPage = 0;
         empty = true;
+        $scope.returnFrom = false;
     }
 
     //模糊查询
@@ -107,7 +119,11 @@ MyFormControllers.controller('MyFormCtrl',function($scope,$compile,CommonService
     $scope.switchTab = function(){
        var aa= CommonService.getAll('order', uriData+nowPage, function (data) {
             $scope.sendHtml(contentTab ,data);
-        });
+        },function(response){
+            if(response.message=="没有找到数据"){
+                $scope.sendHtml(contentTab,null);
+            }
+       });
     }
 
     //删除事件
@@ -144,7 +160,6 @@ MyFormControllers.controller('MyFormCtrl',function($scope,$compile,CommonService
         CommonService.createOne('order/returns', uriData, function (data) {
 
         });
-
     }
 
     //添加数据到页面
@@ -153,16 +168,18 @@ MyFormControllers.controller('MyFormCtrl',function($scope,$compile,CommonService
             $("#"+cid+" tr:gt(0)").remove();
             $("#Prompt").html(null);
         }
-        if(data.OrderList.length==0){      //判断是否有记录
+        if(data==null){      //判断是否有记录
             $("#Prompt").html("<div class='row'><div class='container'><div class='col-md-12'><div class='alert with-icon mp10'>" +
                 "<i class='icon-info-sign'></i><div class='content'>没有符合条件的订单记录。</div></div></div></div></div>");
         }else{
             for(var i=0;i<data.OrderList.length;i++){
+              //  alert(data.OrderList[i][0][0]);
+                localStorage.setItem("da",data.OrderList[i][1]);
                 var Status = data.OrderList[i][0][8]; //交易状态
                 var recordId = data.OrderList[i][0][0]; //记录ID
-                var HTML = "<tr><td><img src='images/products/"+data.OrderList[i][1][0]+"' class='prdouctimg ml10 mr10'/><img src='images/products/"+data.OrderList[i][1][1]+"' class='prdouctimg ml10 mr10'/><img src='images/products/"+data.OrderList[i][1][2]+"' class='prdouctimg ml10 mr10'/></td>"+
+                var HTML = "<tr><td><img src='images/products/"+data.OrderList[i][1][0][1]+"' class='prdouctimg ml10 mr10'/></td>"+
                 "<td>"+data.OrderList[i][0][3]+"</td><td><strong>"+data.OrderList[i][0][4]+"</strong></td><td><strong class='blue'>￥"+data.OrderList[i][0][5]+"</strong></td><td><a class='label label-info'>"+data.OrderList[i][0][7]+"</a></td><td><p class='ha20'>"+data.OrderList[i][0][2].substring(0,10)+"</p><p class='ha20'>"+data.OrderList[i][0][2].substring(11,19)+" </p></td>"+
-                "<td><p class='ha20'><a href='#/viewDetails/"+data.OrderList[i][0][0]+"'>详情</a></p><p class='ha20'><a data-toggle='modal' data-ng-click='deleGoods("+recordId+","+Status+")'>删除</a></p><p class='ha20'><a data-toggle='modal' data-ng-click='vm.activeTab = 5;returnGoods("+data.OrderList[i][0][0]+","+data.OrderList[i][0][1]+")'>退货</a></p></td></tr>";
+                "<td><p class='ha20'><a href='#/viewDetails/"+data.OrderList[i][0][0]+"'>详情</a></p><p class='ha20'><a data-toggle='modal' data-ng-click='deleGoods("+recordId+","+Status+")'>删除</a></p><p class='ha20'><a data-toggle='modal' data-ng-click='vm.activeTab = 5;returnGoods("+data.OrderList[i][0][0]+")'>退货</a></p></td></tr>";
                 var cHTML=$compile(HTML)($scope);  //编译
                 $("#"+cid+" tr:eq(0)").after(cHTML);  //添加至页面
             }
