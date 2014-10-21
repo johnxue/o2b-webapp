ce5efc446f0b806b70d3c4ea34dab62f92f9d4da
chenxinbo <xinbo.chen@jetcloudtech.com>
2014-10-21 19:37:34 +0800

    10.21  去除UEditor多余工具

--- 1.0.0/app/libs/ueditor/ueditor.config.js
+++ 1.0.0/app/libs/ueditor/ueditor.config.js
@@ -30,21 +30,19 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: URL + "jsp/controller.jsp"
-
+       // , serverUrl: URL + "ueditor1_4_3-utf8-php/php/controller.php"
+         //, serverUrl: "http://192.168.1.13/php/controller.php"
+        //,serverUrl: "http://192.168.1.110:8081/o2b/v1.0.0/service/EditHtml"
+        ,serverUrl:"http://192.168.1.110:8081/o2b/v1.0.0/service/EditHtml"
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         , toolbars: [[
-            'fullscreen', 'source', '|', 'undo', 'redo', '|',
-            'bold', 'italic', 'underline', 'fontborder', 'strikethrough', 'superscript', 'subscript', 'removeformat', 'formatmatch', 'autotypeset', 'blockquote', 'pasteplain', '|', 'forecolor', 'backcolor', 'insertorderedlist', 'insertunorderedlist', 'selectall', 'cleardoc', '|',
+            'fullscreen', 'source', '|',
+            'bold', 'italic', 'underline', '|', 'forecolor', 'backcolor', '|',
             'rowspacingtop', 'rowspacingbottom', 'lineheight', '|',
             'customstyle', 'paragraph', 'fontfamily', 'fontsize', '|',
-            'directionalityltr', 'directionalityrtl', 'indent', '|',
-            'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|', 'touppercase', 'tolowercase', '|',
-            'link', 'unlink', 'anchor', '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', '|',
-            'simpleupload', 'insertimage', 'emotion', 'scrawl', 'insertvideo', 'music', 'attachment', 'map', 'gmap', 'insertframe', 'insertcode', 'webapp', 'pagebreak', 'template', 'background', '|',
-            'horizontal', 'date', 'time', 'spechars', 'snapscreen', 'wordimage', '|',
-            'inserttable', 'deletetable', 'insertparagraphbeforetable', 'insertrow', 'deleterow', 'insertcol', 'deletecol', 'mergecells', 'mergeright', 'mergedown', 'splittocells', 'splittorows', 'splittocols', 'charts', '|',
-            'print', 'preview', 'searchreplace', 'help', 'drafts'
+            'justifyleft', 'justifycenter', 'justifyright', 'justifyjustify', '|',
+            'insertimage', 'emotion', 'insertvideo', '|',
+            'preview'
         ]]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //,labelMap:{
@@ -54,7 +52,7 @@
         //语言配置项,默认是zh-cn。有需要的话也可以使用如下这样的方式来自动多语言切换，当然，前提条件是lang文件夹下存在对应的语言文件：
         //lang值也可以通过自动获取 (navigator.language||navigator.browserLanguage ||navigator.userLanguage).toLowerCase()
         //,lang:"zh-cn"
-        //,langPath:URL +"lang/"
+        //,langPath:URL +"ueditor/lang/"
 
         //主题配置项,默认是default。有需要的话也可以使用如下这样的方式来自动多主题切换，当然，前提条件是themes文件夹下存在对应的主题文件：
         //现有如下皮肤:default
